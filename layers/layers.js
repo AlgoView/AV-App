df1987cc8d6b458008815b0e4b33a52ee6a22ff4
michiel-celis-91 <michiel.celis@outlook.com>
2023-11-13 22:28:41 +0100

    Optimized code

--- layers/layers.js
+++ layers/layers.js
@@ -39,129 +39,113 @@ class Layer 												{
 				unfocus										:	function(){
 				}
 			},
-			visibility 										:	{
-				status										:	undefined,
-				show										:	function(){
-					this.status					=	true;
-					$( '#' + __layername + '' ).fadeIn( fadelenght, windowx.rescale() );
+			visibility										: {
+				status										: undefined,
+				show										: function(){
+					this.status 							= true;
+					let element 							= document.getElementById(__layername);
+					element.style.display 					= 'block';
+					element.classList.add('fade-in');
+					windowx.rescale();
 				},
-				hide										:	function(){
-					this.status					=	false;
-					$( '#' + __layername + '' ).fadeOut( fadelenght, windowx.rescale() );
+				hide										: function(){
+					this.status 							= false;
+					let element 							= document.getElementById(__layername);
+					element.style.display 					= 'none';
+					element.classList.add('fade-out');
+					windowx.rescale();
 				},
-				toggle										:	function(){
-					switch ( this.status ){
-						case undefined:	console.log('visibility status undefined');
-						case true:		this.hide();		break;
-						case false:		this.show();		break;
+				toggle										: function(){
+					switch (this.status){
+						case undefined						: console.log('visibility status undefined');
+						case true							: this.hide(); break;
+						case false							: this.show(); break;
 					}
 				},
-				minimize									:	function(){
-					this.visibility.status					=	'none',
-					$( ':root' ).css( '--' + __layername + '-visibility-'				, this.visibility.status		);
+				minimize: function(){
+					this.visibility.status 					= 'none';
+					document.documentElement.style.setProperty('--' + __layername + '-visibility-', this.visibility.status);
 				},
-				maximize									:	function(){
-					this.visibility.status					=	'block',
-					$( ':root' ).css( '--' + __layername + '-visibility-'				, this.visibility.status		);
+				maximize: function(){
+					this.visibility.status 					= 'block';
+					document.documentElement.style.setProperty('--' + __layername + '-visibility-', this.visibility.status);
 				}
 			}
 		};
     };
 };
 
-var layers												=	{
-	getData												:	{},
-	get													:	function(){
-		let ajaxcalls = [];
+var layers 													= {
+	isValidKey												: function(key) {
+		const EXCLUDED_KEYS 								= ["isValidKey","properties","getData","get","set","render"];
+		return !EXCLUDED_KEYS.includes(key);
+	},
+    getData													: {},
+	get														: function(){
+		let self 											= this;
+		let ajaxcalls 										= [];
 		return new Promise((resolve, reject) => {
-			for(var layer in layers){
-				if( ["properties","getData","get","set","render"].indexOf(layer) == -1 ){
-					let ajaxCall = new Promise((resolve, reject) => {
-						$.ajax({
-							layerid: layer,
-							url: 'layers/' + layer + '/' + layer + '.json',
-							dataType: 'json',
-							type: 'GET',
-							success: function(data){
-								layers.getData[this.layerid] = data;
-								resolve(layer);
-							},
-							error: function(error) {
-								reject(error);
-							}
+			Object.keys(self).forEach(layer => {
+				if(self.isValidKey(layer)){
+					let ajaxCall 							= fetch('layers/' + layer + '/' + layer + '.json')
+						.then(response => response.json())
+						.then(data => {
+							self.getData[layer] 			= data;
+							return layer;
 						});
-					});
+	
 					ajaxcalls.push(ajaxCall);
 				}
-			}
+			});
+	
 			Promise.all(ajaxcalls)
 				.then(() => resolve('layers.set()'))
 				.catch(() => reject());
 		});              
 	},
-	set													:	function(){
-		for(var layer in layers){
-			console.log(layer);
-			if( ["properties","getData","get","set","render"].indexOf(layer) == -1 ){
-				layers[layer] = new Layer(layer);
+	set														: function(){
+		Object.keys(layers).forEach(layer => {
+			if(this.isValidKey(layer)){
+				layers[layer] 								= new Layer(layer);
+			}
+		});
+
+		Object.assign(layers, layers.getData);
+	},
+	render													: function(){
+		const layerRoot 									= layers;
+
+		function createDiv(id) {
+			let div 										= document.createElement('div');
+			div.id 											= id;
+			div.style.display 								= 'none';
+			return div;
+		}
+
+		let rootDiv 										= createDiv('layers');
+
+		for(let layer in layerRoot) {
+			if (this.isValidKey(layer)) {
+				let layerDiv 								= createDiv('layers-' + layer);
+				rootDiv.appendChild(layerDiv);
+
+				for(let sublayer in layerRoot[layer]) {
+					if (this.isValidKey(sublayer)) {
+						let sublayerDiv 					= createDiv('layers-' + layer + '-' + sublayer);
+						layerDiv.appendChild(sublayerDiv);
+
+						for(let subsublayer in layerRoot[layer][sublayer]) {
+							if (this.isValidKey(subsublayer)) {
+								let subsublayerDiv 			= createDiv('layers-' + layer + '-' + sublayer + '-' + subsublayer);
+								sublayerDiv.appendChild(subsublayerDiv);
+							}
+						}
+					}
+				}
 			}
 		}
-		_.merge(layers,layers.getData)
 
-		return;
-	},
-	render												:	function(){
-		var append										=	'';
-		var jsid										=	'layers';
-		var htmlid										=	'layers';
-		var layerRoot									=	layers
-
-		// if 		(typeof layers === 'undefined'	) 	{ console.log('Error: layerRoot is undefined'); return; }
-		// else if (typeof layers === 'string'		) 	{ console.log('layerRoot is a string, object expected'); return; }
-		// else if (Object.keys(layers).length == 0) 	{ console.log('layerRoot is an empty object');return; };
-
-		for(var i = 0; i < Object.keys(layerRoot).length; i++) {
-			var layer = Object.keys(layerRoot)[i];
-			
-			jsid = 'layers' + '.' + layer;
-			htmlid = 'layers' + '-' + layer;
-			
-			if( ["properties","getData","get","set","render"].indexOf(layer) == -1 ){
-				console.log("layer: " + layer);
-				append									+=
-				'<div id="' + htmlid + '" style="display:none">';
-
-				for(var sublayer in layerRoot[layer]) {
-					console.log(sublayer);
-					jsid = 'layers' + '.' + layer + '.' + sublayer;
-					htmlid = 'layers' + '-' + layer + '-' + sublayer;
-
-					if( ["properties","getData","get","set","render"].indexOf(sublayer) == -1 ) {
-						console.log("sublayer: " + sublayer);
-						append += '<div id="' + htmlid + '" style="display:none">';
-						append += '</div>';
-
-						for(var subsublayer in layerRoot[layer][sublayer])			{
-							jsid						=	'layers' + '.' + layer + '.' + sublayer + '.' + subsublayer;
-							htmlid 						=	'layers' + '-' + layer + '-' + sublayer + '-' + subsublayer;
-
-							if( ["properties","getData","get","set","render"].indexOf(subsublayer) == -1 )	{
-								console.log("subsublayer: " + subsublayer);
-							append					+= 
-							'<div id="' + htmlid + '" style="display:none">';
-	
-							append					+=
-							'</div>';
-							}
-						}			
-					}
-				};
-				append									+=
-				'</div>';
-			}
-		};
-		$('body').append(append);
-		index.body.construct.status						=	true;
+		document.body.appendChild(rootDiv);
+		index.body.construct.status = true;
 	}
 };
-
